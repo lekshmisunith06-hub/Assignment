e76e8ab2008bc251969cebbe7ae8b5964252bf20
lekshmisunith06-hub <lekshmisunith06@gmail.com>
2026-08-02 23:16:39 +0530

    Add files via upload

---
+++ Lab-1/Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter limit: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        while (a <= n) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
---
+++ Lab-1/gcd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class gcd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+
+        System.out.println("gcd = " + a);
+    }
+}
