f212f4034127c4f0ea57b8e96ec6b25c66ebc3de
lekshmisunith06-hub <lekshmisunith06@gmail.com>
2026-08-03 01:44:03 +0530

    Add files via upload

---
+++ Tutorial-3/noevenodd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class noevenodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int even = 0, odd = 0;
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        System.out.println("Even elements = " + even);
+        System.out.println("Odd elements = " + odd);
+    }
+}
